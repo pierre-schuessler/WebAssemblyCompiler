42d6830c42504cb88cabf5671db677b6573f32e7
Pierre Schüssler <98694986+pierre-schuessler@users.noreply.github.com>
2026-05-15 23:35:15 +0200

    Replaced ; with . in path

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Add entries here to expose alternative compiler / editor backends in the UI.
 
 const COMPILER_SOURCES = [
-  { id: "lang_1_compiler_v2",  label: "Language v1 Compiler v2", path: ";/compilers/lang_1.js" },
+  { id: "lang_1_compiler_v2",  label: "Language v1 Compiler v2", path: "./compilers/lang_1.js" },
   { id: 'lang_2_compiler_v1', label: "Language v2 Compiler v1", path: "./compiler.js"}
 ];
 
